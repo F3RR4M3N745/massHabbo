e6dd88ed4ea63d83538d40a4c93fa6c93b1fb7c4
F3RR4M3N745 <F3RR4M3N745@hotmail.com>
2015-07-05 13:13:05 -0300

    #

---
+++ bypass.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  </head>
+
+ <body>
+ 	<script type="text/javascript">
+    var myCallback = function( myParam ) {
+        alert( "this was called across domains!" + myParam );
+    };
+</script>
+</body>
+
+</html>
